783c1956a925e999ff9011ec16a9db513c75abce
Gabriel Gonzalez <gabrielgonzalez@gmail.com>
2026-03-09 19:23:06 -0600

    enrique

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("enriqueeee")
+const express = require("express")
+const colors = require("colors")
+const dotenv = require("dotenv").config()
+const connectDB = require('./config/db')
+const port = process.env.PORT || 5000
+
+connectDB()
+
+const app = express()
+
+app.listen(port, () => console.log(`Servidor Iniciado en el puerto: ${port}`))
