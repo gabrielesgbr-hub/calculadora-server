955e8993e680f0dbce8061f69e8823789f18d30c
Gabriel Gonzalez <gabrielgonzalez@gmail.com>
2026-03-09 20:14:20 -0600

    moo flashback

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,15 @@ const colors = require("colors")
 const dotenv = require("dotenv").config()
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+const {errorHandler} = require ('./middleware/errorMiddleware')
 
 connectDB()
 
 const app = express()
+
+app.use(express.json())
+app.use('/api/movimientos', require('./routes/movimientosRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Servidor Iniciado en el puerto: ${port}`))
